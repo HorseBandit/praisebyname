c979101005a06d8b8f791b1f3a406ca1cb572968
Jerrod Estell <jrodmath1@gmail.com>
2024-02-21 20:01:35 -0800

    commit

---
+++ Interfaces/IZombieComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermOneSWE.Interfaces
+{
+    interface IZombieComponent
+    {
+        string Type { get; }
+        int Health { get; }
+        void TakeDamage(int damage);
+        void Die();
+    }
+
+}
---
+++ Interfaces/IZombieFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermOneSWE.Interfaces
+{
+    interface IZombieFactory
+    {
+        IZombieComponent CreateZombie(string type);
+    }
+
+}
